dd45e7a3ba359151f50bb39784bd57ba676df81d
Daniel Gaspar Gonçalves <daniel.gaspar.goncalves@gmail.com>
2023-04-27 15:50:19 -0300

    modelagem usando regressao logistica

--- android-app/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ android-app/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,9 +7,12 @@ import android.bluetooth.BluetoothGattCallback
 import android.bluetooth.BluetoothGattCharacteristic
 import android.bluetooth.BluetoothSocket
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.icu.text.SimpleDateFormat
+import android.net.Uri
 import android.os.*
+import android.text.InputType
 import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.style.ForegroundColorSpan
@@ -18,8 +21,11 @@ import android.view.View
 import android.widget.*
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import java.io.BufferedReader
 import java.io.File
 import java.io.FileOutputStream
+import java.io.FileReader
+import java.nio.file.Paths
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -29,8 +35,9 @@ class MainActivity : AppCompatActivity() {
     private var connected: Boolean = false
     private val messages = mutableListOf<String>()
     private val messagesSpannable = SpannableStringBuilder()
+    private val data = mutableListOf<String>()
     private lateinit var connectButton: Button
-    private lateinit var textView: TextView
+    private lateinit var textViewBT: TextView
 
     @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,13 +45,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         connectButton = findViewById(R.id.btn_connect)
-        textView = findViewById(R.id.text_view)
+        textViewBT = findViewById(R.id.text_view_bt)
 
         bluetoothService = BluetoothService(this)
 
         // desativar sub-menus
         var linearLayout: LinearLayout
-        for (i in listOf<Int>(R.id.layout_bt, R.id.layout_about)) {
+        for (i in listOf<Int>(R.id.layout_bt, R.id.layout_data, R.id.layout_about)) {
             linearLayout = findViewById<LinearLayout>(i)
             linearLayout.isEnabled = false
             linearLayout.visibility = View.INVISIBLE
@@ -58,10 +65,10 @@ class MainActivity : AppCompatActivity() {
         var linearLayout: LinearLayout
 
         // desativa layout ativo
-        for (i in listOf<Int>(R.id.layout_bt, R.id.layout_about)) {
+        for (i in listOf<Int>(R.id.layout_bt, R.id.layout_data, R.id.layout_about)) {
             linearLayout = findViewById<LinearLayout>(i)
             linearLayout.isEnabled = false
-            linearLayout.visibility = View.INVISIBLE
+            linearLayout.visibility = View.GONE
         }
 
         // ativa layout atual
@@ -70,6 +77,17 @@ class MainActivity : AppCompatActivity() {
                 linearLayout = findViewById<LinearLayout>(R.id.layout_bt)
                 linearLayout.isEnabled = true
                 linearLayout.visibility = View.VISIBLE
+            }
+            R.id.btn_content_table -> {
+                linearLayout = findViewById<LinearLayout>(R.id.layout_data)
+                linearLayout.isEnabled = true
+                linearLayout.visibility = View.VISIBLE
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    listData()
+                } else {
+                    Toast.makeText(this, "Esta funcionalidade requer a versao Android 'O' ou superior", Toast.LENGTH_SHORT).show()
+                }
             }
             R.id.btn_about -> {
                 linearLayout = findViewById<LinearLayout>(R.id.layout_about)
@@ -93,7 +111,7 @@ class MainActivity : AppCompatActivity() {
                     connected = true
                     connectButton.text = "Desconectar"
                 } else {
-                    Toast.makeText(this, "Esta funcionalidade requer a versao Android N ou superior", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Esta funcionalidade requer a versao Android 'N' ou superior", Toast.LENGTH_SHORT).show()
                 }
             }
         } else {
@@ -122,14 +140,14 @@ class MainActivity : AppCompatActivity() {
         // imprime no console do aplicativo
         println(dataTimeMessage)
         //textView.text = messages.joinToString(separator = "") // Sem alterar a cor
-        textView.text = messagesSpannable // alterando a cor
+        textViewBT.text = messagesSpannable // alterando a cor
     }
 
     fun clearBT (v: View) {
         messages.clear()
         messagesSpannable.clear() // alterando a cor
-        //textView.text = messages.joinToString(separator = "") // Sem alterar a cor
-        textView.text = messagesSpannable
+        //textViewBT.text = messages.joinToString(separator = "") // Sem alterar a cor
+        textViewBT.text = messagesSpannable
     }
 
     @RequiresApi(Build.VERSION_CODES.N)
@@ -157,7 +175,7 @@ class MainActivity : AppCompatActivity() {
                         messages.clear()
                         messagesSpannable.clear() // alterando a cor
                         //textView.text = messages.joinToString(separator = "") // Sem alterar a cor
-                        textView.text = messagesSpannable
+                        textViewBT.text = messagesSpannable
                     }
                 } catch (e: Exception) {
                     e.printStackTrace()
@@ -169,4 +187,60 @@ class MainActivity : AppCompatActivity() {
 
         filenameDialog.show()
     }
+
+    // ---------------------------------------------------------------------------------------------
+    // Data
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun listData () {
+        val path = getExternalFilesDir(null)
+
+        if (path != null) {
+            val files = path.listFiles()
+
+            for (file in files) {
+                if (file.isFile) {
+                    val reader = BufferedReader(FileReader(file))
+                    var line = reader.readLine()
+
+                    while (line != null) {
+                        if (!data.contains(line)) {
+                            data.add(line)
+                        }
+
+                        line = reader.readLine()
+                    }
+
+                    reader.close()
+                }
+            }
+        }
+
+        val textView: TextView = findViewById(R.id.text_view_data)
+        textView.text = data.joinToString(separator = "\n")
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun exportData (v: View) {
+        val emailInputDialog = AlertDialog.Builder(this)
+        emailInputDialog.setTitle("Enviar arquivo de texto")
+        emailInputDialog.setMessage("Insira o e-mail do destinatário:")
+        val input = EditText(this)
+        input.inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+        emailInputDialog.setView(input)
+        emailInputDialog.setPositiveButton("Enviar") { _, _ ->
+            val recipient = input.text.toString()
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            emailIntent.type = "text/plain"
+            emailIntent.data = Uri.parse("mailto:")
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(recipient))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Weather Prediction")
+            emailIntent.putExtra(Intent.EXTRA_TEXT, data.joinToString(separator = "\n"))
+
+            startActivity(Intent.createChooser(emailIntent, "Enviar email"))
+        }
+        emailInputDialog.setNegativeButton("Cancelar") { dialog, _ -> dialog.cancel() }
+        emailInputDialog.show()
+    }
+
 }
